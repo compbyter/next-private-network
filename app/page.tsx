9f1806b986a275a52a12f5ccfc68a0c2ced16bf4
compbyter <2twofurkan@gmail.com>
2026-01-08 17:05:42 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { client } from '@/client';
 export const dynamic = 'force-dynamic';
 const getData = async () => {
-  const homepage = client.single('homepage');
-  const defaultHomepage = await homepage.find();
-  console.log('Homepage: ', defaultHomepage);
-  return defaultHomepage;
+  try {
+    const homepage = client.single('homepage');
+    const defaultHomepage = await homepage.find();
+    console.log('Homepage: ', defaultHomepage);
+    return defaultHomepage;
+  } catch (error: any) {
+    console.error('Failed fetch homepage data: ', error?.response?.message);
+    return null;
+  }
 };
 
 export default async function Home() {
   const data = await getData();
-  return <div>Homepage title: {data?.data?.homepageTitle}</div>;
+  return <div>Homepage title: {data && data?.data?.homepageTitle}</div>;
 }
